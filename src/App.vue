6ede4a628ef4f7f72f7da7637d855325d7939664
lee hom <102334679+leehom22@users.noreply.github.com>
2025-11-16 17:12:46 +0800

    Project Setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import { Button } from './components/ui/button';
+
+</script>
+
+<template>
+  <Button class="text-red-500">Hello World</Button>
+</template>
+
+<style scoped>
+
+
+</style>
