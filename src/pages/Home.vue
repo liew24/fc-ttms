112141563b452e47c342d48aea1f1d823eb5e77e
lee hom <102334679+leehom22@users.noreply.github.com>
2025-11-22 18:38:13 +0800

    Setup chart js

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,4 +1,5 @@
 <script setup>
+import BarChart from "@/components/chart/BarChart.vue";
 import { Button } from "@/components/ui/button"; // shadcn Button import
 import { useUserStore } from "@/stores/user";
 import { ChartLine, GraduationCap, House, LibraryBig, School, Sheet, Users, UserStar } from "lucide-vue-next";
@@ -32,6 +33,7 @@ const goToPages = (page) => {
             <h1 class="text-2xl font-bold mb-4">Welcome Back {{ user && user.name }}!</h1>
             <p class="text-gray-700">
                 This is a simple homepage with a navbar and a logout button using shadcn components.
+                <BarChart/>
             </p>
         </main>
     </div>
--- src/pages/Student.vue
+++ src/pages/Student.vue
@@ -1,4 +1,5 @@
 <template>
+    <!-- Lee Hom -->
     <div>
         Lecturer page
     </div>
